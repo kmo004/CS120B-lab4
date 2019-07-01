77573ba3a8eb4ffa3105a0cfa4aab265a22498a8
kmo004 <kmo004@ucr.edu>
2019-06-30 22:46:09 -0700

    ex2

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: kmo004
  *  Partner(s) Name: Moker(Ke) Bellomo
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab # 4 Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,104 +12,77 @@
  #include "simAVRHeader.h"
  #endif
 
+enum states {START, INIT, PLUS, MINUS, RESET} state;
+
 int main(void){
+	
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x01;
-	enum states {Start, INIT, OFF, ON, WAIT1, WAIT2} state;
-	state = Start;
+	DDRC = 0xFF; PORTC = 0x07;
+	state = START;
 	
 	while(1){
-		switch(state){ 
-		case Start:
-		PORTB = 0x01;
+		tick();
+	}
+	return 1;
+}
+
+
+
+void tick(){
+	switch(state){
+		case START:
 		state = INIT;
 		break;
 		
 		case INIT:
-		if((~PINA & 0x01) == 0x01){
-			state = OFF; 
-			break;
+		if(PINA == 0){
+			state = RESET;
+		}
+		else if (PINA == 1){
+			state = PLUS;
+		}
+		else if (PINA == 2){
+			state = MINUS;
 		}
 		else{
-			state = INIT; 
-			break;
+			state = INIT;
 		}
-		
-		case OFF:
-		if((~PINA & 0x01) == 0x00){
-			state = WAIT1; 
-			break;
-		}
-		else
-		{
-			state = OFF; 
-			break;
-		}
-			
-		case ON:
-		if((~PINA & 0x01) == 0x00)
-		{
-			state = WAIT2; 
-			break;
-		}
-		else
-		{
-			state = ON; 
-			break;
-		}
-		
-		case WAIT1:
-		if((~PINA & 0x01) == 0x01)
-		{
-			state = ON; 
-			break;
-		}
-		else
-		{
-			state = WAIT1; 
-			break;
-		}
-		
-		case WAIT2:
-		if((~PINA & 0x01) == 0x01)
-		{
-			state = OFF; 
-			break;
-		}
-		else
-		{
-			state = WAIT2; 
-			break;
-		}
-		
-		default:
 		break;
+		
+		case PLUS:
+		state = INIT;
+		break;
+		
+		case MINUS:
+		state = INIT;
+		break;
+		
+		case RESET:
+		state = INIT;
+		break;
+	}
+	switch(state){
+		case START:
+		break;
+		
+		case INIT:
+		break;
+		
+		case PLUS:
+		if(PORTC < 9){
+			PORTC = PORTC + 1;
 		}
-		switch(state){ 
-			case Start:
-			break;
+		break;
 		
-			case INIT:
-			PORTB = 0x01;
-			break;
-		
-			case OFF:
-			PORTB = 0x02;
-			break;
-		
-			case ON:
-			PORTB = 0x01;
-			break;
-		
-			case WAIT1:
-			break;
-		
-			case WAIT2:
-			break;
-		
-			default:
-			break;
+		case MINUS:
+		if(PORTC > 0){
+			PORTC = PORTC - 1;
 		}
+		break;
+		
+		case: RESET:
+			PORTC = 0x00;
+		break;
 	}
 }
 
