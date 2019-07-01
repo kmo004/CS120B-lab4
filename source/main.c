9352451d8febf8b3380ea652021e8b7d532645bb
kmo004 <kmo004@ucr.edu>
2019-06-30 23:19:10 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,88 +1,113 @@
 /*	Author: kmo004
  *  Partner(s) Name: Moker(Ke) Bellomo
  *	Lab Section:
- *	Assignment: Lab # 4 Exercise #2
+ *	Assignment: Lab # 4 Exercise #3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
- #include <avr/io.h>
- #ifdef _SIMULATE_
- #include "simAVRHeader.h"
- #endif
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+enum STATES { START, INIT, S1, S2, S3, OPEN} state;
 
-enum STATES { START, INIT, PLUS, MINUS, RESET} state;
+int main(void) {
 
-int main(void){
+	DDRA= 0xFF; PORTA=0x00;
+	DDRB= 0x00; PORTB = 0xFF;
 	
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTC = 0x07;
-	state = START;
 	
-	while(1){
+	while (1) {
 		tick();
 	}
+	
 	return 1;
 }
 
-
-
 void tick(){
-	switch(state){
+	
+	switch(state) {
 		case START:
 		state = INIT;
 		break;
 		
 		case INIT:
-		if(PINA == 0){
-			state = RESET;
+		if(PINA == 1){
+			state= S1;
 		}
-		else if (PINA == 1){
-			state = PLUS;
+		else{
+			state= INIT;
 		}
-		else if (PINA == 2){
-			state = MINUS;
+		break;
+		
+		case S1:
+		if(PINA == 1){
+			state = S1;
+		}
+		else if(PINA == 0){
+			state= S2;
+		}
+		else{
+			state= INIT;
+		}
+		break;
+		
+		case S2:
+		if (PINA == 0){
+			state = S2;
+		}
+		else if(PINA == 2){
+			state = S3;
+		}
+		else {
+			state= INIT;
+		}
+		break;
+		
+		case S3:
+		if (PINA == 2){
+			state = S3;
+		}
+		else if (PINA == 0){
+			state= OPEN;
 		}
 		else{
 			state = INIT;
 		}
 		break;
 		
-		case PLUS:
-		state = INIT;
-		break;
-		
-		case MINUS:
-		state = INIT;
-		break;
-		
-		case RESET:
-		state = INIT;
+		case OPEN:
+		if (PINA == 0x80){
+			state = INIT;
+		}
+		else{
+			state = OPEN;
+		}
 		break;
 	}
-	switch(state){
+	
+	switch(state) {
 		case START:
 		break;
 		
 		case INIT:
+		PORTB= 0x00;
 		break;
 		
-		case PLUS:
-		if(PORTC < 9){
-			PORTC = PORTC + 1;
-		}
+		case S1:
 		break;
 		
-		case MINUS:
-		if(PORTC > 0){
-			PORTC = PORTC - 1;
-		}
+		case S2:
 		break;
 		
-		case: RESET:
-			PORTC = 0x00;
+		case S3:
 		break;
+		
+		case OPEN:
+		PORTB = 0x01;
+		break;
+		
 	}
 }
-
